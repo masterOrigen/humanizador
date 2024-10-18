9a389530db6dba89dfadb32d377e05239000ad55
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-10-18 13:58:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def humanizar_texto(texto):
     
     payload = {
         "language": "auto",
-        "strength": 4,  # Nivel fijo en 4 para máxima reescritura
+        "strength": 3,  # Nivel fijo en 4 para máxima reescritura
         "text": texto
     }
     
